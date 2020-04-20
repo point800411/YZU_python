9e577165f33380c898619df91d7f53f431dc2939
Da Vinci <point800411@gmail.com>
2020-04-20 22:46:19 +0800

    Lesson 2 review1

--- lesson02/Cass 09.py
+++ lesson02/Cass 09.py
@@ -2,7 +2,12 @@ w = int(input('請輸入體重:'))
 h = int(input('請輸入升高:'))
 BMI = w / (h/100) ** 2
 
-while BMI > 23 :
+if BMI > 23 :
     R = "過胖"
-
-print("身高: %.1f 體重: %.1 BMI= %.12f Result= %s")
+elif BIM < 18 :
+    R = "過瘦"
+else:
+    R = "正常"
+print(w, h, BMI, R)
+print("身高: %.1f 體重: %.1f BMI= %.2f Result= %s"%
+      (w, h, BMI, R))
---
+++ lesson02/Cass BMI.py
@@ -0,0 +1,1 @@
+def BMI(x, y):
